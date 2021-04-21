422a2e00927f15917d03848e2b21009b34f75c53
nextsigner <nextsigner@gmail.com>
2021-04-21 05:38:27 -0300

    se optimiza jday2.py

--- resources/jday2.py
+++ resources/jday2.py
@@ -2,7 +2,15 @@ import jdutil
 import datetime
 import sys
 
-print(sys.argv[1:])
+dia = sys.argv[1]
+mes = sys.argv[2]
+anio = sys.argv[3]
+hora = sys.argv[4]
+min = sys.argv[5]
 
-d = datetime.datetime(1975,6,20,23)  
+#msg1 = "Fecha" + dia + "/" + mes
+print('Fecha: '+dia+'/'+mes+'/'+anio+' Hora: '+hora+':'+min)
+
+
+d = datetime.datetime(int(anio),int(mes),int(dia),int(hora), int(min))
 print(jdutil.datetime_to_jd(d))
