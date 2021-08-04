88ecd1d773bdd7de61163857ac46f2fe5f05d3c2
nextsigner <nextsigner@gmail.com>
2021-08-04 11:08:19 -0300

    se agrega unik.pro main.cpp y unik.qrc

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "unikqprocess.h"
+#include "unik.h"
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    //UnikQProcess uqp;
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("./main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    qmlRegisterType<UnikQProcess>("unik.UnikQProcess", 1, 0, "UnikQProcess");
+    Unik u;
+    engine.rootContext()->setContextProperty("unik", &u);
+    engine.load(url);
+
+    return app.exec();
+}
